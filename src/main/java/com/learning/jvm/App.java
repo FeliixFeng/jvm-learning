b6e1adc75a5ca36811b1d9be0124cd598b0fd78c
feliixfeng <3288733739@qq.com>
2026-01-27 17:24:12 +0800

    Initial JVM learning project structure

---
+++ src/main/java/com/learning/jvm/App.java
@@ -0,0 +1,13 @@
+package com.learning.jvm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
