15135cf0207e43bd5fe77bf9439885dd6f9f138b
feliixfeng <3288733739@qq.com>
2026-01-27 22:44:40 +0800

    day01: jvm组成

---
+++ src/main/java/com/learning/jvm/Demo1.java
@@ -0,0 +1,11 @@
+package com.learning.jvm;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = i+ 1;
+        System.out.println(j);
+        i = i ++;
+        System.out.println(i);
+    }
+}
---
+++ src/main/java/com/learning/jvm/Demo2.java
@@ -0,0 +1,12 @@
+package com.learning.jvm;
+
+/**
+ * arthas测试案例
+ */
+public class Demo2 {
+    public static void main(String[] args) throws InterruptedException {
+        while(true){
+            Thread.sleep(1000L);
+        }
+    }
+}
